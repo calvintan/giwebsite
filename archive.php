70c28655602393e05fd087118639dbb377aba1a6
Calvin Tan <hello@calvin-tan.com>
2019-12-05 01:21:25 +0800

    Separate the files into template parts

--- archive.php
+++ archive.php
@@ -2,9 +2,15 @@
 
 <div class="container">
   <div class="row">
+    <header class="col">
+      <?php the_archive_title('<h1>', '</h1>'); ?>
+      <?php the_archive_description('<p>', '</p>'); ?>
+    </header>
+  </div>
+  <div class="row">
     <div class="col-md-<?php echo is_active_sidebar('primary-sidebar') ? '8' : '12'; ?>">
       <main role="main">
-        <?php get_template_part('loop', 'index'); ?>
+        <?php get_template_part('loop', 'archive'); ?>
       </main>
     </div>
     <?php if(is_active_sidebar('primary-sidebar')) { ?>
