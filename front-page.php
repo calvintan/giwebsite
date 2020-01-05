06033f857a4d14336b83752c736c9bc9d4d27640
Calvin Tan <hello@calvin-tan.com>
2020-01-05 21:19:22 +0800

    Adjust carousel, organise styles

--- front-page.php
+++ front-page.php
@@ -1,18 +1,35 @@
 <?php get_header(); ?>
 
-<div id="carousel-homepage" class="carousel slide" data-ride="carousel">
+<div id="carousel-homepage" class="carousel slide mb-5" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-homepage" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-homepage" data-slide-to="1"></li>
+    <li data-target="#carousel-homepage" data-slide-to="2"></li>
+    <li data-target="#carousel-homepage" data-slide-to="3"></li>
+  </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <div class="carousel-content">
+      <div class="container">
         <p>Eleven years of initiating positive changes towards a greener China</p>
       </div>
     </div>
     <div class="carousel-item">
-      <div class="carousel-content">
-        <p>test</p>
+      <div class="container">
+        <p>Eleven years of eco-learning and actions for a greener China</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="container">
+        <p>700+ community and corporate activities attended by 30,000+ people</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="container">
+        <p>We are creating a world where individuals can live healthily, in harmony with nature</p>
       </div>
     </div>
   </div>
+  <!--
   <a class="carousel-control-prev" href="#carousel-homepage" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
@@ -21,13 +38,14 @@
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
+  -->
 </div>
 
 <div class="container">
   <div class="row">
     <div class="col-md-12">
       <main role="main">
-        <?php get_template_part('loop', 'page'); ?>
+        <?php /* get_template_part('loop', 'page'); */ ?>
       </main>
     </div>
   </div>
