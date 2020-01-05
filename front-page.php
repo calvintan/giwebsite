8c99209b22d88764f000aa7164b832d209000ef3
Calvin Tan <hello@calvin-tan.com>
2020-01-05 19:04:13 +0800

    Add carousel

--- front-page.php
+++ front-page.php
@@ -1,5 +1,28 @@
 <?php get_header(); ?>
 
+<div id="carousel-homepage" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <div class="carousel-content">
+        <p>Eleven years of initiating positive changes towards a greener China</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <div class="carousel-content">
+        <p>test</p>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carousel-homepage" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carousel-homepage" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
 <div class="container">
   <div class="row">
     <div class="col-md-12">
