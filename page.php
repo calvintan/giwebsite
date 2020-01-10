5ac73db8c4ccc03071140caacd359f4a30af6be1
Calvin Tan <hello@calvin-tan.com>
2020-01-10 10:55:31 +0800

    Temporarily hide sidebar from being displayed

--- page.php
+++ page.php
@@ -1,14 +1,11 @@
 <?php get_header(); ?>
 
-<div class="container">
+<div class="container my-5">
   <div class="row">
-    <div class="col-md-8">
+    <div class="col-md-12">
       <main role="main">
         <?php get_template_part('loop', 'page'); ?>
       </main>
-    </div>
-    <div class="col-md-4">
-      <?php get_sidebar(); ?>
     </div>
   </div>
 </div>
