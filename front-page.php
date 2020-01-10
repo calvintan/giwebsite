2b290d77dc9f9438b411c9455f79643ca447ff97
Calvin Tan <hello@calvin-tan.com>
2020-01-10 10:59:31 +0800

    Display up to 4 event posts on the homepage

--- front-page.php
+++ front-page.php
@@ -46,13 +46,41 @@
     <div class="col-md-12">
       <main role="main">
         <?php get_template_part('loop', 'page'); ?>
+        <?php
+          $event_exists = post_type_exists( 'event' );
+          $project_exists = post_type_exists( 'project' );
+        ?>
         <div class="lists-homepage">
-          <h2>Projects</h2>
-          <p>Projects goes here</p>
+          <?php if($event_exists) { ?>
+            <h2>Latest Events</h2>
+            <?php 
+              $args = array( 
+                'post_type' => 'event',
+                'posts_per_page' => 4,
+                'order' => 'DESC',
+                'orderby' => 'date' 
+              );
+              $the_query = new WP_Query( $args ); 
+            ?>
+
+            <?php if($the_query->have_posts()) { ?>
+              <?php while($the_query->have_posts()) { ?>
+                <?php $the_query->the_post(); ?>
+                <?php get_template_part('template-parts/post/content', 'event'); ?>
+              <?php } ?>
+              <?php the_posts_pagination(); ?>
+              <?php wp_reset_postdata(); ?>
+            <?php } else { ?>
+              <?php get_template_part('template-parts/post/content', 'none'); ?>
+            <?php } ?>
+            <a href="<?php echo home_url() ?>/events">View All Events</a>
+          <?php } ?>
         </div>
         <div class="lists-homepage">
-          <h2>Events</h2>
-          <p>Events goes here</p>
+          <?php if($project_exists) { ?>
+          <h2>Latest Projects</h2>
+          <p>Projects goes here</p>
+          <?php } ?>
         </div>
       </main>
     </div>
