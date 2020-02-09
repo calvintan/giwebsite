f070b0f3432bf509c2045950c710ab9e81e8b583
Calvin Tan <hello@calvin-tan.com>
2020-02-10 00:04:23 +0800

    Style the news page

--- loop.php
+++ loop.php
@@ -1,7 +1,13 @@
 <?php if(have_posts()) { ?>
   <?php while(have_posts()) { ?>
     <?php the_post(); ?>
-    <?php get_template_part('template-parts/post/content'); ?>
+
+    <?php if(is_home()) { ?>
+      <?php get_template_part('template-parts/page/content', 'cards'); ?>
+    <?php } else { ?>
+      <?php get_template_part('template-parts/post/content'); ?>
+    <?php } ?>
+    
   <?php } ?>
   <?php the_posts_pagination(); ?>
 <?php } else { ?>
--- template-parts/page/content.php
+++ template-parts/page/content.php
@@ -1,11 +1,12 @@
 <article>
-  <header class="c-page__header">
-    <?php if(!is_front_page()) { ?>
-      <h1 class="c-page__title">
-        <?php the_title(); ?>
-      </h1>
-    <?php } ?>
+  <?php if(!is_front_page()) { ?>
+  <header class="page__header">
+    <h1 class="page__title">
+      <?php the_title(); ?>
+    </h1>
   </header>
+  <?php } ?>
+  
   <div class="c-page__content">
     <?php the_content(); ?>
   </div>
