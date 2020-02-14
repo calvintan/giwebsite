be7ffac5468283ab79390f86ac40e383f6c79a3a
Calvin Tan <hello@calvin-tan.com>
2020-02-14 23:55:18 +0800

    Add copyright to footer

--- footer.php
+++ footer.php
@@ -14,6 +14,20 @@
           <?php dynamic_sidebar( 'footer_four' ); ?>
         </div>
       </div>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="f-copyright">
+            <div class="f-copyright__left">
+              <span>Designed by <a href="#">Matter</a></span>
+              <span>Developed by <a href="#">Codesmiths</a></span>
+            </div>
+            <div class="f-copyright__center"></div>
+            <div class="f-copyright__right">
+              <span>沪ICP备123456号 &copy; <?php echo date( 'Y' ) ?> Green Initiatives</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </footer>
   <?php wp_footer(); ?>
