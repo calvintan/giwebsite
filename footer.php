7a6d5118070b2318ab5ead3e24f70139ab20eae0
Calvin Tan <hello@calvin-tan.com>
2020-02-14 22:36:48 +0800

    Update footer column

--- footer.php
+++ footer.php
@@ -1,16 +1,16 @@
   <footer class="footer-homepage py-5">
     <div class="container">
       <div class="row">
-        <div class="col-sm-3">
+        <div class="col-md-3">
           <?php dynamic_sidebar( 'footer_one' ); ?>
         </div>
-        <div class="col-sm-3">
+        <div class="col-md-3">
           <?php dynamic_sidebar( 'footer_two' ); ?>
         </div>
-        <div class="col-sm-3">
+        <div class="col-md-3">
           <?php dynamic_sidebar( 'footer_three' ); ?>
         </div>
-        <div class="col-sm-3">
+        <div class="col-md-3">
           <?php dynamic_sidebar( 'footer_four' ); ?>
         </div>
       </div>
