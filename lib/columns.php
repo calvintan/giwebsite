077f69c6f43d18b75da6fd25366c855be6c2e90f
Calvin Tan <hello@calvin-tan.com>
2020-02-17 00:02:36 +0800

    Add columns for event, project and partner posts in the admin

--- lib/columns.php
+++ lib/columns.php
@@ -1,23 +1,18 @@
 <?php
-  /*
-  * Add columns to member post list
-  */
-  function add_acf_columns ( $columns ) {
+  /* Custom columns for Member post type */
+  function add_member_acf_columns ( $columns ) {
     return array_merge ( $columns, array ( 
       'photo' => __ ( 'Photo' ),
       'member_type' => __ ( 'Type' ),
-      'member_position'   => __ ( 'Postion' ) 
+      'member_position'   => __ ( 'Position' ) 
     ) );
   }
-  add_filter ( 'manage_member_posts_columns', 'add_acf_columns' );
+  add_filter ( 'manage_member_posts_columns', 'add_member_acf_columns' );
 
-  /*
-  * Add columns to member post list
-  */
   function member_custom_column ( $column, $post_id ) {
     switch ( $column ) {
       case 'photo' :
-        echo get_the_post_thumbnail( $post_id, 'profile-small' );
+        echo get_the_post_thumbnail( $post_id, 'profile-size' );
         break;
       case 'member_type':
         echo get_post_meta ( $post_id, 'member_type', true );
@@ -29,10 +24,6 @@
   }
   add_action ( 'manage_member_posts_custom_column', 'member_custom_column', 10, 2 );
 
-  /*
-  * Add columns to member post list
-  */
-
   function set_custom_member_sortable_columns( $columns ) {
     $columns['member_type'] = 'member_type';
 
@@ -40,4 +31,80 @@
   }
 
   add_filter( 'manage_edit-member_sortable_columns', 'set_custom_member_sortable_columns' );
+
+  /* Custom columns for Event post type */
+  function add_event_acf_columns ( $columns ) {
+    return array_merge ( $columns, array ( 
+      'photo' => __ ( 'Photo' ),
+      'event_date' => __ ( 'Date' ),
+      'event_start_time' => __ ( 'Start Time' ),
+      'event_end_time'   => __ ( 'End Time' ) 
+    ) );
+  }
+  add_filter ( 'manage_event_posts_columns', 'add_event_acf_columns' );
+
+  function event_custom_column ( $column, $post_id ) {
+    switch ( $column ) {
+      case 'photo' :
+        echo get_the_post_thumbnail( $post_id, 'feature-size' );
+        break;
+      case 'event_date':
+        $date = get_post_meta ( $post_id, 'event_date', true );
+        echo date('j F Y', strtotime($date));
+        break;
+      case 'event_start_time':
+        echo get_post_meta ( $post_id, 'event_start_time', true );
+        break;
+      case 'event_end_time':
+        echo get_post_meta ( $post_id, 'event_end_time', true );
+        break;
+    }
+  }
+  add_action ( 'manage_event_posts_custom_column', 'event_custom_column', 10, 2 );
+
+  /* Custom columns for Project post type */
+  function add_project_acf_columns ( $columns ) {
+    return array_merge ( $columns, array ( 
+      'photo' => __ ( 'Photo' ),
+      'project_start_date' => __ ( 'Start Date' ),
+      'project_end_date'   => __ ( 'End Date' ) 
+    ) );
+  }
+  add_filter ( 'manage_project_posts_columns', 'add_project_acf_columns' );
+
+  function project_custom_column ( $column, $post_id ) {
+    switch ( $column ) {
+      case 'photo' :
+        echo get_the_post_thumbnail( $post_id, 'feature-size' );
+        break;
+      case 'project_start_date':
+        $start_date = get_post_meta ( $post_id, 'project_start_date', true );
+        echo date('j F Y', strtotime($start_date));
+        break;
+      case 'project_end_date':
+        $end_date = get_post_meta ( $post_id, 'project_end_date', true );
+        echo date('j F Y', strtotime($end_date));
+        break;
+    }
+  }
+  add_action ( 'manage_project_posts_custom_column', 'project_custom_column', 10, 2 );
+
+  /* Custom columns for Partner post type */
+  function add_partner_acf_columns ( $columns ) {
+    return array_merge ( $columns, array ( 
+      'partner_logo' => __ ( 'Logo' )
+    ) );
+  }
+  add_filter ( 'manage_partner_posts_columns', 'add_partner_acf_columns' );
+
+  function partner_custom_column ( $column, $post_id ) {
+    switch ( $column ) {
+      case 'partner_logo' :
+        $image_id = get_post_meta ( $post_id, 'partner_logo', true );
+        $image = wp_get_attachment_image_src($image_id, 'logo-size');
+        echo '<img width="'.$image[1].'" height="'.$image[2].'" src='.$image[0].'>';
+        break;
+    }
+  }
+  add_action ( 'manage_partner_posts_custom_column', 'partner_custom_column', 10, 2 );
 ?>
--- lib/theme-support.php
+++ lib/theme-support.php
@@ -3,7 +3,9 @@
 function _themename_theme_support() {
   add_theme_support( 'title-tag' );
 	add_theme_support( 'post-thumbnails' );
-	add_image_size('profile-small', 80, 80, true);
+	add_image_size('feature-size', 160, 70, true);
+	add_image_size('profile-size', 80, 80, true);
+	add_image_size('logo-size', 100, 100, false);
   add_theme_support( 'html5', array(
 			'search-form',
 			'comment-form',
