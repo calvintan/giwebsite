a10541724fcd74cd501d942771d6796351db8725
Calvin Tan <hello@calvin-tan.com>
2020-02-27 11:47:52 +0800

    Add shadow to cards on hover

--- template-parts/page/content-cards.php
+++ template-parts/page/content-cards.php
@@ -1,37 +1,39 @@
 <article <?php post_class('c-post mb-4 col-md-4'); ?>>
-  <?php if(get_the_post_thumbnail() !== '') { ?>
-    <div class="c-post__thumbnail">
-      <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-        <?php the_post_thumbnail('large'); ?>
-      </a>
-    </div>
-  <?php } ?>
-
-  <?php if('event' == get_post_type()) { ?>
-    <?php
-      $date_string = get_field('event_date');
-      $date = DateTime::createFromFormat('d/m/Y', $date_string);
-    ?>
-    <div class="c-post__card-info">
-      <ul>
-        <li>
-          <span>
-            <?php echo $date->format('l'); ?>
-          </span>
-        </li>
-        <li>
-          <span>
-            <?php echo $date->format('j F Y'); ?>
-          </span>
-        </li>
-      </ul>
-    </div>
-  <?php } ?>
-
   <div class="c-post__card">
-    <h3 class="c-post__title">
-      <?php the_title();?>
-    </h3>
-    <p><?php echo get_excerpt(); ?></p>
+    <?php if(get_the_post_thumbnail() !== '') { ?>
+      <div class="c-post__thumbnail">
+        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+          <?php the_post_thumbnail('large'); ?>
+        </a>
+      </div>
+    <?php } ?>
+
+    <?php if('event' == get_post_type()) { ?>
+      <?php
+        $date_string = get_field('event_date');
+        $date = DateTime::createFromFormat('d/m/Y', $date_string);
+      ?>
+      <div class="c-post__card-info">
+        <ul>
+          <li>
+            <span>
+              <?php echo $date->format('l'); ?>
+            </span>
+          </li>
+          <li>
+            <span>
+              <?php echo $date->format('j F Y'); ?>
+            </span>
+          </li>
+        </ul>
+      </div>
+    <?php } ?>
+
+    <div class="c-post__card-desc">
+      <h3 class="c-post__title">
+        <?php the_title();?>
+      </h3>
+      <p><?php echo get_excerpt(); ?></p>
+    </div>
   </div>
 </article>
--- template-parts/post/content-partner.php
+++ template-parts/post/content-partner.php
@@ -9,22 +9,23 @@
   <div class="row">
   <?php foreach( $projects as $project): ?>
     <article <?php post_class('c-post mb-4 col-md-4'); ?>>
-      <div class="c-post__thumbnail">
-        <?php echo get_the_post_thumbnail($project->ID, 'large'); ?>
-      </div>
-
       <div class="c-post__card">
-        <h3 class="c-post__title">
-          <a href="<?php the_permalink( $project->ID ); ?>">
-            <?php echo get_the_title( $project->ID ); ?></a>
-        </h3>
-        <?php
-          $excerpt = get_the_excerpt($project->ID);
-          $excerpt = substr($excerpt, 0, 60);
-          $excerpt = substr($excerpt, 0, strripos($excerpt, " "));
-          $result = $excerpt.'... <a href="'.get_the_permalink($project->ID).'">more</a>';
-        ?>
-        <p><?php echo $result; ?></p>
+        <div class="c-post__thumbnail">
+          <a href="<?php the_permalink( $project->ID ); ?>"><?php echo get_the_post_thumbnail($project->ID, 'large'); ?></a>
+        </div>
+
+        <div class="c-post__card-desc">
+          <h3 class="c-post__title">
+            <?php echo get_the_title( $project->ID ); ?>
+          </h3>
+          <?php
+            $excerpt = get_the_excerpt($project->ID);
+            $excerpt = substr($excerpt, 0, 60);
+            $excerpt = substr($excerpt, 0, strripos($excerpt, " "));
+            $result = $excerpt.'... <a href="'.get_the_permalink($project->ID).'">more</a>';
+          ?>
+          <p><?php echo $result; ?></p>
+        </div>
       </div>
     </article>
   <?php endforeach; ?>
