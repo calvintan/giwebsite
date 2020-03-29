4932437d6d1318504f6860880ef3efdbfb1d51f2
Calvin Tan <hello@calvin-tan.com>
2020-03-29 15:32:07 +0800

    Only display search results for post, event, project

--- lib/helpers.php
+++ lib/helpers.php
@@ -50,10 +50,18 @@ function share_buttons() {
   include( locate_template('share-template.php', false, false) );
 }
 
-function change_select_title($cat_args){
-$cat_args['show_option_none'] = __('Filter');
-return $cat_args;
+function change_select_title($cat_args) {
+  $cat_args['show_option_none'] = __('Filter');
+  return $cat_args;
 }
 add_filter('widget_categories_dropdown_args', 'change_select_title');
+
+function search_filter($query) {
+  if ($query->is_search && !is_admin() ) {
+    $query->set('post_type', array('post', 'event', 'project'));
+  }
+  return $query;
+}
+add_filter('pre_get_posts', 'search_filter');
 
 ?>
