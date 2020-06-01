455bac24ff76b908bfa55a62eacb679ce989ff18
Calvin Tan <hello@calvin-tan.com>
2020-06-01 14:23:36 +0800

    Link to languages folder for translations

--- functions.php
+++ functions.php
@@ -7,4 +7,9 @@ require_once('lib/theme-support.php');
 require_once('lib/navigation.php');
 require_once('lib/columns.php');
 
+function _themename_load_textdomain() {
+    load_theme_textdomain('_themename', get_template_directory() . '/languages');
+}
+add_action('after_setup_theme', '_themename_load_textdomain');
+
 ?>
