072d6248453428da38fb2851fb4b10281c71e040
Calvin Tan <hello@calvin-tan.com>
2020-06-04 14:16:05 +0800

    Update homepage layout

--- front-page.php
+++ front-page.php
@@ -34,78 +34,97 @@
 </div>
 <?php wp_reset_postdata(); ?>
 
-<div class="container my-5">
-  <div class="row">
-    <div class="col-md-12">
-      <main role="main">
-        <?php get_template_part('loop', 'page'); ?>
-        <?php
-          $event_exists = post_type_exists( 'event' );
-          $project_exists = post_type_exists( 'initiative' );
-          $event_count = wp_count_posts('event') -> publish;
-          $project_count = wp_count_posts('initiative') -> publish;
-        ?>
-        <div class="lists-homepage">
-          <?php if($event_exists && $event_count > 0) { ?>
-          <h2>Events</h2>
-          <div class="lists-wrapper">
-            <?php
-              $args = array(
-                'post_type' => 'event',
-                'posts_per_page' => 4,
-                'meta_key'  => 'event_date',
-                'orderby' => 'meta_value_num',
-                'order' => 'DESC'
-              );
-              $the_query = new WP_Query( $args );
-            ?>
-
-            <?php if($the_query->have_posts()) { ?>
-              <?php while($the_query->have_posts()) { ?>
-                <?php $the_query->the_post(); ?>
-                <?php get_template_part('template-parts/page/content', 'list'); ?>
-              <?php } ?>
-              <?php the_posts_pagination(); ?>
-              <?php wp_reset_postdata(); ?>
-            <?php } else { ?>
-              <?php get_template_part('template-parts/post/content', 'none'); ?>
-            <?php } ?>
-          </div>
-          <a class="lists-link" href="<?php echo home_url() ?>/events">View All Events</a>
-          <?php } ?>
+<main role="main">
+  <section class="homepage-top">
+    <div class="container">
+      <?php get_template_part('loop', 'page'); ?>
+      <!-- <div class="row">
+        <div class="col-md-4">
+          <h3>What we do</h3>
         </div>
-        <div class="lists-homepage">
-          <?php if($project_exists && $project_count > 0) { ?>
-          <h2>Initiatives</h2>
-          <div class="lists-wrapper">
-            <?php
-              $args = array(
-                'post_type' => 'initiative',
-                'posts_per_page' => 4,
-                'meta_key'  => 'project_end_date',
-                'orderby' => 'meta_value_num',
-                'order' => 'DESC'
-              );
-              $the_query = new WP_Query( $args );
-            ?>
-
-            <?php if($the_query->have_posts()) { ?>
-              <?php while($the_query->have_posts()) { ?>
-                <?php $the_query->the_post(); ?>
-                <?php get_template_part('template-parts/page/content', 'list'); ?>
-              <?php } ?>
-              <?php the_posts_pagination(); ?>
-              <?php wp_reset_postdata(); ?>
-            <?php } else { ?>
-              <?php get_template_part('template-parts/post/content', 'none'); ?>
-            <?php } ?>
-          </div>
-          <a class="lists-link" href="<?php echo home_url() ?>/initiatives">View All Initiatives</a>
-          <?php } ?>
+        <div class="col-md-8">
+          <h2>We deliver programs and initiatives for organisations on long-term value creation to maximise their positive environmental impact.</h2>
         </div>
-      </main>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <span class="hp-top-icon">
+            <img src="<?php bloginfo('stylesheet_directory'); ?>/dist/assets/images/home-top-1.svg" alt="">
+          </span>
+        </div>
+        <div class="col-md-8">
+          <h3>Waste &amp; Recycling</h3>
+          <p>We are helping both individuals and businesses to reduce and optimise their waste and take responsibility for their consumption by connecting them to smart, sustainable, and data-driven waste and recycling solutions.</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4 order-md-last">
+          <span class="hp-top-icon">
+            <img src="<?php bloginfo('stylesheet_directory'); ?>/dist/assets/images/home-top-2.svg" alt="">
+          </span>
+        </div>
+        <div class="col-md-8 order-md-first">
+          <h3>Thought Leadership</h3>
+          <p>We are helping companies to be 'eco-leaders' and environmentally aware for their employees and customers by facilitating activities and programs that change behaviours and help inform sustainable business decisions.</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <span class="hp-top-icon">
+            <img src="<?php bloginfo('stylesheet_directory'); ?>/dist/assets/images/home-top-3.svg" alt="">
+          </span>
+        </div>
+        <div class="col-md-8">
+          <h3>Social Business Support</h3>
+          <p>We are assisting social entrepreneurs and businesses increase their market presence by enabling the proliferation and commercialisation of sustainable solutions, technologies and platforms to create scalable impact.</p>
+        </div>
+      </div> -->
     </div>
-  </div>
-</div>
+  </section>
+  <section class="homepage-bot">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <h3>Our Approach</h3>
+        </div>
+        <div class="col-md-8">
+          <h2>We understand that businesses want to be more responsible towards their employees, society, and the environment, yet the internal business fabric varies. This affects implementation format and program roll-out. To support these constraints, our initiatives are:</h2>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="uneven-grid">
+        <div class="uneven-left">
+          <div class="uneven-left-top">
+            <div class="uneven-title">
+              <img src="<?php bloginfo('stylesheet_directory'); ?>/dist/assets/images/home-bot-mod.svg" alt="">
+              <h3>Modular + Customisable</h3>
+            </div>
+            <p>All our initiatives are developed to be implemented in bite-sized chunks over a specific timeline. We create custom solutions, building on the success of our existing programs.</p>
+          </div>
+          <div class="uneven-left-bottom">
+            <div class="uneven-title">
+              <img src="<?php bloginfo('stylesheet_directory'); ?>/dist/assets/images/home-bot-collab.svg" alt="">
+              <h3>Collaborative</h3>
+            </div>
+            <p>We work with our clients to identify these requirements, and then propose tailored partners in our community to ensure effective delivery of solutions.</p>
+          </div>
+        </div>
+        <div class="uneven-right">
+          <div class="uneven-right-top">
+            <div class="uneven-title">
+              <img src="<?php bloginfo('stylesheet_directory'); ?>/dist/assets/images/home-bot-data.svg" alt="">
+              <h3>Data Driven</h3>
+            </div>
+            <p>What cannot be measured, cannot be improved. Gathering data and measuring impact are important to us. We benchmark against industry standards, set targets, and achieve them. It allows our clients to communicate with confidence and transparency the imapact they have made.</p>
+          </div>
+          <div class="uneven-right-bottom">
+            <p>All our initiatives follow necessary legal standards and are backed by various environmental policies, corporate responsibility guidelines, ESG frameworks and recommendations for companies by the Chinese Government as well as international organisations.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
 
 <?php get_footer(); ?>
--- template-parts/page/content.php
+++ template-parts/page/content.php
@@ -1,13 +1,11 @@
-<article>
-  <?php if(!is_front_page()) { ?>
-  <header class="page__header">
-    <h1 class="page__title">
-      <?php the_title(); ?>
-    </h1>
-  </header>
-  <?php } ?>
-  
-  <div class="c-page__content">
-    <?php the_content(); ?>
-  </div>
-</article>
+<?php if(!is_front_page()) { ?>
+<header class="page__header">
+  <h1 class="page__title">
+    <?php the_title(); ?>
+  </h1>
+</header>
+<?php } ?>
+
+<div class="c-page__content">
+  <?php the_content(); ?>
+</div>
