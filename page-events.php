bf0a6bd830f46ec25b8193b6131ad4fefa534544
Calvin Tan <hello@calvin-tan.com>
2020-06-14 02:10:45 +0800

    Display up to 6 items at a time, trigger pagination

--- page-events.php
+++ page-events.php
@@ -16,12 +16,14 @@
       </div>
 
       <?php
+        $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
         $date_args = array(
           'post_type'   => 'event',
           'meta_key' => 'event_date',
-          'posts_per_page' => -1,
+          'posts_per_page' => 6,
+          'paged'          => $paged,
           'orderby' => 'meta_value_num',
-          'order' => 'DESC',
+          'order' => 'ASC',
           'meta_query'=> array(
               array(
                 'key' => 'event_date',
@@ -34,6 +36,13 @@
         $the_query = new WP_Query( $date_args );
       ?>
 
+      <?php
+        // Pagination fix
+        $temp_query = $wp_query;
+        $wp_query   = NULL;
+        $wp_query   = $the_query;
+      ?>
+
       <?php if($the_query->have_posts()) { ?>
         <?php while($the_query->have_posts()) { ?>
           <?php $the_query->the_post(); ?>
@@ -41,6 +50,12 @@
         <?php } ?>
         <?php the_posts_pagination(); ?>
         <?php wp_reset_postdata(); ?>
+
+        <?php
+          // Reset main query object
+          $wp_query = NULL;
+          $wp_query = $temp_query;
+        ?>
       <?php } else { ?>
         <?php get_template_part('template-parts/post/content', 'none'); ?>
       <?php } ?>
@@ -52,7 +67,7 @@
       $pastZh = pll_get_post($past->ID, 'zh');
     ?>
     <div class="row">
-      <div class="col-md-12">
+      <div class="col-md-12 mt-4">
         <?php if(pll_current_language() == 'en') { ?>
           <a class="past-events-link" href="<?php echo esc_url( get_permalink( $pastEn ) ); ?>"><?php esc_html_e( 'View all past events', '_themename' ); ?></a>
         <?php } else { ?>
