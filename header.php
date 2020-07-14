c422cc387cb2386f96c332e82c35bbf5e07b3f54
Calvin Tan <hello@calvin-tan.com>
2020-07-14 17:00:38 +0800

    Add fontawesome cdn, add favicons

--- header.php
+++ header.php
@@ -5,6 +5,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <?php wp_head(); ?>
+  <link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/favicon.ico" />
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('stylesheet_directory'); ?>/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo('stylesheet_directory'); ?>/favicon-16x16.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('stylesheet_directory'); ?>/apple-touch-icon.png">
+  <script src="https://kit.fontawesome.com/a0108bb788.js" crossorigin="anonymous"></script>
 </head>
 <body <?php body_class(); ?>>
 <div id="manifesto">
